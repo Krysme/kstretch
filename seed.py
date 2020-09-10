895cfed31f69d6afc8b2752fb88ea936b856a74f
Krys <blackshop.s.bossss@gmail.com>
2020-09-11 01:59:25 +0800

    added 12 words bip39 scheme

--- seed.py
+++ seed.py
@@ -1,3 +1,5 @@
+#!/bin/python
+
 from mnemonic import Mnemonic
 from monero.seed import Seed
 import kstretch
@@ -13,6 +15,10 @@ bip39_seed = bip39.to_mnemonic(bytes.fromhex(seed_hex))
 
 print("bip39 seed: " + bip39_seed)
 
+bip39_12 = Mnemonic("english")
+bip39_seed_12 = bip39.to_mnemonic(bytes.fromhex(seed_hex[0:len(seed_hex) // 2]))
+
+print("bip39 seed 12: " + bip39_seed_12)
 
 
 
